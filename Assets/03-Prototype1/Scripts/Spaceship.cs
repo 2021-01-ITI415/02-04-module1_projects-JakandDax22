bd64b024807f9ed19ba55829459578dac3f82016
JakandDax22 <60167934+JakandDax22@users.noreply.github.com>
2021-02-17 19:04:10 -0500

    Changed camera and added spaceship movement

--- Assets/03-Prototype1/Scripts/Spaceship.cs
+++ Assets/03-Prototype1/Scripts/Spaceship.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
+    //Create the laser prefab
+    public GameObject laserPrefab;
+
+    //Spaceship speed
+    public float speed = 2f;
+
+    //Indicate distance of where the ship will change direction 
+    public float leftAndRightEdge = 10f;
+
+    //Chance of changing direction
+    public float chanceToChangeDirections = 0.1f;
+
+    //Set the frequency of laser blasts
+    public float secondsbetweenLaser = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set the time in beween laser blasts
+        Invoke("laserBlast", 2f);
+
+    }
+
+    void laserBlast()
+    {
+        GameObject laser = Instantiate<GameObject>(laserPrefab);
+        laser.transform.position = transform.position;
+        Invoke("laserBlast", secondsbetweenLaser);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Change Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //Move left
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //Change direction
+        }
     }
 }
+
